59089c86ae13f39b06c234c3125a8b2e9b6ca355
bouzuya <m@bouzuya.net>
2019-08-04 15:00:49 +0900

    add path params to BbnService.getEntry

--- app/src/main/java/net/bouzuya/bbn_entry/BbnService.kt
+++ app/src/main/java/net/bouzuya/bbn_entry/BbnService.kt
@@ -1,8 +1,13 @@
 package net.bouzuya.bbn_entry
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BbnService {
-    @GET("/2019/08/01/index.json")
-    suspend fun getEntry(): Entry
+    @GET("/{year}/{month}/{date}/index.json")
+    suspend fun getEntry(
+        @Path("year") year: String,
+        @Path("month") month: String,
+        @Path("date") date: String
+    ): Entry
 }
--- app/src/main/java/net/bouzuya/bbn_entry/EntryRepository.kt
+++ app/src/main/java/net/bouzuya/bbn_entry/EntryRepository.kt
@@ -1,5 +1,5 @@
 package net.bouzuya.bbn_entry
 
 class EntryRepository(private val bbnService: BbnService) {
-    suspend fun getEntry() = bbnService.getEntry()
+    suspend fun getEntry() = bbnService.getEntry("2019", "08", "01")
 }
