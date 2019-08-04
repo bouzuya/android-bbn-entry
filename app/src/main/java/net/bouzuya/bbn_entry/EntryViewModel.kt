720678c14471ce8105e4df26bd670df5669cc21d
bouzuya <m@bouzuya.net>
2019-08-04 15:14:29 +0900

    add progress_bar to entry_fragment, and fix layout

--- app/src/main/java/net/bouzuya/bbn_entry/EntryViewModel.kt
+++ app/src/main/java/net/bouzuya/bbn_entry/EntryViewModel.kt
@@ -8,14 +8,19 @@ import kotlinx.coroutines.launch
 import org.threeten.bp.LocalDate
 
 class EntryViewModel(private val entryRepository: EntryRepository) : ViewModel() {
+    private val _fetching = MutableLiveData<Boolean>()
+    val fetching: LiveData<Boolean> = _fetching
+
     val name = "Entry"
 
     private val _title = MutableLiveData<String>()
     val title: LiveData<String> = _title
 
     fun fetch() = viewModelScope.launch {
+        _fetching.postValue(true)
         val yesterday = LocalDate.now().minusDays(1)
         val entry = entryRepository.getEntry(yesterday)
         _title.postValue(entry.title)
+        _fetching.postValue(false)
     }
 }
