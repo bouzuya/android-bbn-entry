f94a25714a5da358ee26f1a2ec856ea6f93418ad
bouzuya <m@bouzuya.net>
2019-08-04 15:10:34 +0900

    add com.jakewharton.threetenabp:threetenabp:1.2.1

--- app/src/main/java/net/bouzuya/bbn_entry/BbnEntryApplication.kt
+++ app/src/main/java/net/bouzuya/bbn_entry/BbnEntryApplication.kt
@@ -1,10 +1,16 @@
 package net.bouzuya.bbn_entry
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class BbnEntryApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+
     fun getEntryRepository(): EntryRepository = EntryRepository(
         Retrofit.Builder()
             .baseUrl("https://blog.bouzuya.net")
